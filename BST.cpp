5f0c8ff14f677730bbd4a3aacb01d76ed7d56be8
tejas <tejasshah@gmail.com>
2023-09-20 10:00:51 +0530

    search bst

--- BST.cpp
+++ BST.cpp
@@ -20,7 +20,7 @@ Node *addNode(Node *root, int data) // 60,80   70,80 NULL,80
     }
     else
     {
-        if (root->data < data)
+        if (root->data > data)
         {
             // left
             root->left = addNode(root->left, data); // NULL,80
@@ -34,11 +34,61 @@ Node *addNode(Node *root, int data) // 60,80   70,80 NULL,80
     return root;
 }
 
+void inOrder(Node *root) // 60
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);       // left  inOrder(50)
+        printf(" %d", root->data); // root
+        inOrder(root->right);      // right
+    }
+}
+
+// pre ->  root left  right
+// post -> left right root
+// in   -> left root  right
+
+void preOrder(Node *root) // 60
+{
+    if (root != NULL)
+    {
+        printf(" %d", root->data); // root
+        preOrder(root->left);      // left  inOrder(50)
+        preOrder(root->right);     // right
+    }
+}
+int search(Node *root, int key)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else if (root->data == key)
+    {
+        // logic delete
+        return 1;
+    }
+    else if (root->data > key)
+    {
+        return search(root->left, key);
+    }
+    else
+    {
+        return search(root->right, key);
+    }
+}
 int main()
 {
     Node *root = NULL;
     root = addNode(root, 60);
     addNode(root, 70); // 60,70
     addNode(root, 80); // 60 , 80
+    addNode(root, 50);
+
+    inOrder(root); // 60
+
+    search(root, 40); // no present
+    search(root, 80); // present
+
     return 0;
 }
